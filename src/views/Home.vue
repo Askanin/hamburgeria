9341cebcaba604f4363cfaa53fdc014a595aff72
Felipe Lima <flimas1985@gmail.com>
2024-08-15 14:01:02 -0300

    Paginas de home e pedidos com seus componentes

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  
+  <Banner />
+  
+  <div class="main-container">
+    <h1>HOME</h1>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+  import Banner from '../components/Banner.vue'
+  import BurgerForm from '../components/BurgerForm.vue'
+
+  export default {
+    name: "Home",
+    components: {
+      Banner,
+      BurgerForm
+    }
+  }
+
+
+</script>
